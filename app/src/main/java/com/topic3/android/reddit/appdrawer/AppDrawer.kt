fa666b981e6ab5fef812459b10bfbb36f65cc619
IvanHrozny2 <96377288+IvanHrozny2@users.noreply.github.com>
2023-05-24 20:27:25 +0300

    Change ProfileInfo

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -2,16 +2,15 @@ package com.topic3.android.reddit.appdrawer
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -20,8 +19,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import com.topic3.android.reddit.R
+
 import com.topic3.android.reddit.theme.RedditThemeSettings
+import kotlinx.coroutines.NonDisposableHandle.parent
+
 /**
  * Представляет корневую композицию для панели приложений, используемой на экранах.
  */
@@ -61,13 +65,64 @@ private fun AppDrawerHeader() {
       contentDescription = stringResource(
         id = R.string.account)
     )
+    Text(
+      text = stringResource(R.string.default_username),
+      color = MaterialTheme.colors.primaryVariant
+    )
   }
+  Divider(
+    color = MaterialTheme.colors.onSurface.copy(alpha = .2f),
+    modifier = Modifier.padding(
+      start = 16.dp, end = 16.dp, top = 16.dp
+    )
+  )
 }
 
 @Composable
-fun ProfileInfo() {
+  fun ProfileInfo(modifier: Modifier = Modifier) {
   //TODO add your code here
-}
+    ConstraintLayout(
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(top = 16.sp)
+    ){
+      val (karmaItem, divider, ageItem) = createRefs()
+      val colors = MaterialTheme.colors
+
+      ProfileInfoItem(
+        Icons.Filled.Star,
+        R.string.default_karma_amount,
+        R.string.karma,
+        modifier = Modifier.constrainAs(karmaItem){
+          centerVerticallyTo(parent)
+          start.linkTo(parent.start)
+        }
+      )
+
+      Divider(
+        modifier = Modifier
+          .width(1.dp)
+          .constrainAs(divider){
+            centerVerticallyTo(karmaItem)
+            centerHorizontallyTo(parent)
+            height = Dimension.fillToConstraints
+          },
+        color = colors.onSurface.copy(alpha = .2f)
+      )
+      ProfileInfoItem(Icons.Filled.ShoppingCart,
+        R.string.default_reddit_age_amount,
+        R.string.reddit_age,
+        modifier = Modifier.constrainAs(ageItem){
+          start.linkTo(divider.end)
+          centerVerticallyTo(parent)
+        }
+      )
+
+
+    }
+
+  }
+
 @Composable
 private fun ProfileInfoItem(
   iconAsset: ImageVector,
