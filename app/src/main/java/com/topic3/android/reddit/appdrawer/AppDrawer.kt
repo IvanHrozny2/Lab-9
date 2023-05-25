baa730d8cd308ca601337060f494839af964a0b3
IvanHrozny2 <96377288+IvanHrozny2@users.noreply.github.com>
2023-05-25 15:52:09 +0300

    Change AppDrawer.kt(AppDrawerBody)

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -5,18 +5,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,32 +49,6 @@ fun AppDrawer(
 @Composable
 private fun AppDrawerHeader() {
   //TODO add your code here
-  Column(
-    modifier = Modifier.fillMaxWidth(),
-    horizontalAlignment = Alignment.CenterHorizontally
-  ){
-    Image(
-      imageVector = Icons.Filled.AccountCircle,
-      colorFilter = ColorFilter.tint(Color.LightGray),
-      modifier = Modifier
-        .padding(16.dp)
-        .size(50.dp),
-      contentScale = ContentScale.Fit,
-      alignment = Alignment.Center,
-      contentDescription = stringResource(
-        id = R.string.account)
-    )
-    Text(
-      text = stringResource(R.string.default_username),
-      color = MaterialTheme.colors.primaryVariant
-    )
-  }
-  Divider(
-    color = MaterialTheme.colors.onSurface.copy(alpha = .2f),
-    modifier = Modifier.padding(
-      start = 16.dp, end = 16.dp, top = 16.dp
-    )
-  )
 }
 
 @Composable
